861969a1dbacb47eae5bc4c7bb782ea00353a853
Tallys Martins <tallysmartins@gmail.com>
2020-02-29 22:30:33 -0300

    Adds changeset definitions to Complain module
    Signed-off-by: Tallys Martins <tallysmartins@gmail.com>

--- apps/trustvox/lib/trustvox/complains/complain.ex
+++ apps/trustvox/lib/trustvox/complains/complain.ex
@@ -1,5 +1,7 @@
 defmodule Trustvox.Complains.Complain do
   use Ecto.Schema
+  import Ecto.Changeset
+  alias Trustvox.Complains.Complain
 
   schema "complains" do
     field :title, :string
@@ -8,5 +10,19 @@ defmodule Trustvox.Complains.Complain do
     field :company, :string
 
     timestamps(type: :utc_datetime)
+  end
+
+  @fields [
+    :title,
+    :description,
+    :locale,
+    :company,
+  ]
+
+  @required_fields @fields -- [:locale, :company]
+  def changeset(%Complain{} = complain, attrs) do
+    complain
+    |> cast(attrs, @fields)
+    |> validate_required(@required_fields)
   end
 end
